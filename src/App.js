0f72624f0c1915d42b0828ef74e2c3108afc7bd3
sevilodiana <135428308+sevilodiana@users.noreply.github.com>
2023-06-03 20:23:14 +0200

    Create one movie card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useLayoutEffect} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import SearchIcon from './search.svg';
+
+//435facab
+
+const API_URL = 'http://www.omdbapi.com?apikey=435facab'
+
+const movie1 = {
+  "Title": "Shrek the Third",
+  "Year": "2007",
+  "imdbID": "tt0413267",
+  "Type": "movie",
+  "Poster": "https://m.media-amazon.com/images/M/MV5BOTgyMjc3ODk2MV5BMl5BanBnXkFtZTcwMjY0MjEzMw@@._V1_SX300.jpg"
+}
+
+const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+      searchMovies('Shrek');
+    }, []);
+
+    return (
+      <div className="app">
+        <h1>MovieLand</h1>
+
+        <div className="search"> 
+          <input 
+            placeholder="Search for movies"
+            value="Superman"
+            onChange={() => {}}
+          />
+          <img 
+            src={SearchIcon}
+            alt="search"
+            onClick={()=>{}}
+          />
+        </div>
+
+      <div className="container">
+        <div className="movie">
+            <div>
+              <p>{movie1.Year}</p>
+            </div>
+
+            <div>
+             <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.Title}/>
+            </div>
+
+            <div>
+              <span>{movie1.Type}</span>
+              <h3>{movie1.Title}</h3>
+            </div> 
+          </div>
+        </div>
+      </div>
+    );
 }
 
 export default App;
